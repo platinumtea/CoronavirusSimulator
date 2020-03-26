4f2f69f44282b229b9c3dc7deec9b76198c8f385
platinumtea <platinumtea7@gmail.com>
2020-03-26 12:49:58 -0400

    Update Corona.java

--- src/Corona.java
+++ src/Corona.java
@@ -1,4 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Corona {
-
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Coronavirus Simulation");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.getContentPane().add(new CoronaPanel());
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
