d33955d2f1e759c2c1d414e0408226bc0c8a0a77
platinumtea <platinumtea7@gmail.com>
2020-03-27 21:26:27 -0400

    Moved graph data to a 2D array
    Moved graph data from 5 arrays to a 2D array, and stopped counting dead.

--- src/CoronaPanel.java
+++ src/CoronaPanel.java
@@ -10,24 +10,21 @@ public class CoronaPanel extends JPanel {
 	private int tick, timerTriggers;
 
 	Person[] people;
-	int[] healthy, infected, hospitalized, recovered, dead;
+	int[][] graph;
 
 	public CoronaPanel() {
 		timerTriggers = 0;
 		tick = 0;
 		people = new Person[PEOPLE];
-		healthy = new int[WIDTH];
-		infected = new int[WIDTH];
-		hospitalized = new int[WIDTH];
-		recovered = new int[WIDTH];
-		dead = new int[WIDTH];
+		graph = new int[4][WIDTH];
+
 		int infected = (int) (Math.random() * 100);
 		for (int i = 0; i < people.length; i++) {
 			people[i] = new Person((int) (Math.random() * (WIDTH - 10)), (int) (Math.random() * (HEIGHT - 10)),
 					i == infected ? true : false);
 		}
 		timer = new Timer(DELAY, new CoronaListener());
-		setPreferredSize(new Dimension(WIDTH, HEIGHT + (PEOPLE / GRAPH_SCALING)));
+		setPreferredSize(new Dimension(WIDTH * 2, HEIGHT + (PEOPLE / GRAPH_SCALING)));
 		setBackground(Color.white);
 		timer.start();
 	}
@@ -38,29 +35,22 @@ public class CoronaPanel extends JPanel {
 			switch (people[i].getStatus()) {
 			case 0:
 				page.setColor(Color.green);
-				if (timerTriggers % RECORD_RATE == 0)
-					healthy[tick]++;
 				break;
 			case 1:
 				page.setColor(Color.red);
-				if (timerTriggers % RECORD_RATE == 0)
-					infected[tick]++;
 				break;
 			case 2:
 				page.setColor(Color.pink);
-				if (timerTriggers % RECORD_RATE == 0)
-					hospitalized[tick]++;
 				break;
 			case 3:
 				page.setColor(Color.blue);
-				if (timerTriggers % RECORD_RATE == 0)
-					recovered[tick]++;
 				break;
 			case 4:
 				page.setColor(Color.gray);
-				if (timerTriggers % RECORD_RATE == 0)
-					dead[tick]++;
 				break;
+			}
+			if (timerTriggers % RECORD_RATE == 0 && people[i].getStatus() != 4) {
+				graph[people[i].getStatus()][tick]++;
 			}
 			page.fillOval(people[i].getX(), people[i].getY(), 10, 10);
 			for (int j = 0; j < people.length; j++) {
@@ -73,20 +63,20 @@ public class CoronaPanel extends JPanel {
 			int temp = HEIGHT + (PEOPLE / GRAPH_SCALING);
 
 			page.setColor(Color.red);
-			page.drawLine(i, temp - (infected[i] / GRAPH_SCALING), i, temp);
-			temp -= (infected[i] / GRAPH_SCALING);
+			page.drawLine(i, temp - (graph[1][i] / GRAPH_SCALING), i, temp);
+			temp -= (graph[1][i] / GRAPH_SCALING);
 
 			page.setColor(Color.green);
-			page.drawLine(i, temp - (healthy[i] / GRAPH_SCALING), i, temp);
-			temp -= (healthy[i] / GRAPH_SCALING);
-
+			page.drawLine(i, temp - (graph[0][i] / GRAPH_SCALING), i, temp);
+			temp -= (graph[0][i] / GRAPH_SCALING);
+			
 			page.setColor(Color.pink);
-			page.drawLine(i, temp - (hospitalized[i] / GRAPH_SCALING), i, temp);
-			temp -= (hospitalized[i] / GRAPH_SCALING);
+			page.drawLine(i, temp - (graph[2][i] / GRAPH_SCALING), i, temp);
+			temp -= (graph[2][i] / GRAPH_SCALING);
 
 			page.setColor(Color.blue);
-			page.drawLine(i, temp - (recovered[i] / GRAPH_SCALING), i, temp);
-			temp -= (recovered[i] / GRAPH_SCALING);
+			page.drawLine(i, temp - (graph[3][i] / GRAPH_SCALING), i, temp);
+			temp -= (graph[3][i] / GRAPH_SCALING);
 
 			page.setColor(Color.gray);
 			page.drawLine(i, HEIGHT, i, temp);
@@ -105,12 +95,7 @@ public class CoronaPanel extends JPanel {
 			}
 			if (tick > 599) {
 				tick = 0;
-				healthy = new int[WIDTH];
-				infected = new int[WIDTH];
-				hospitalized = new int[WIDTH];
-				recovered = new int[WIDTH];
-				dead = new int[WIDTH];
-				System.out.println("RESET");
+				graph = new int[4][WIDTH];
 			}
 		}
 	}
