d94c02cbce54c3d8fd20d40a4e19fef6672edbb1
platinumtea <platinumtea7@gmail.com>
2020-03-28 19:26:32 -0400

    Disabled resizing of frame

--- src/Corona.java
+++ src/Corona.java
@@ -1,13 +1,16 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class Corona {
 	static JFrame frame = new JFrame("Coronavirus Simulation");
+
 	public static void main(String[] args) {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		frame.getContentPane().add(new CoronaPanel());
 		frame.pack();
 		frame.setVisible(true);
-		frame.setMinimumSize(frame.getSize());
+		frame.setMinimumSize(new Dimension(CoronaPanel.WIDTH, CoronaPanel.HEIGHT));
+		frame.setResizable(false);
 	}
 }
