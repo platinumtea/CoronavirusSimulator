b9ade6248ed33109254ea1dbf6cd081745dedd64
platinumtea <platinumtea7@gmail.com>
2020-03-28 20:26:54 -0400

    Added record rate, capacity, and distancing sliders

--- src/CoronaPanel.java
+++ src/CoronaPanel.java
@@ -6,8 +6,8 @@ import javax.swing.event.ChangeListener;
 
 @SuppressWarnings("serial")
 public class CoronaPanel extends JPanel {
-	private final int DELAY = 100, PEOPLE = 300, RECORD_RATE = 2, IMMOBILE = 200, CAPACITY = 50, LAYOUT_WIDTH = 200;
-	private int graphScale = 2, numPeople = PEOPLE;
+	private final int DELAY = 10, PEOPLE = 300, RECORD_RATE = 2, IMMOBILE = 200, CAPACITY = 50, LAYOUT_WIDTH = 200;
+	private int graphScale = 2, numPeople = PEOPLE, recordRate = RECORD_RATE, immobile = IMMOBILE, capacity = CAPACITY;
 	static final int WIDTH = 600, HEIGHT = 800;
 	Timer timer;
 	private int tick, timerTriggers;
@@ -17,8 +17,8 @@ public class CoronaPanel extends JPanel {
 	int[][] graph, graphDistanced;
 
 	private JPanel controls;
-	private JSlider scaleSlider, timerSlider, peopleSlider;
-	private JLabel controlLabel, scaleLabel, timerLabel, peopleLabel;
+	private JSlider scaleSlider, timerSlider, peopleSlider, recordSlider, immobileSlider, capacitySlider;
+	private JLabel controlLabel, scaleLabel, timerLabel, peopleLabel, recordLabel, immobileLabel, capacityLabel;
 	private JButton pauseButton, restartButton, playButton;
 
 	public CoronaPanel() {
@@ -86,6 +86,42 @@ public class CoronaPanel extends JPanel {
 		peopleLabel = new JLabel("People: " + PEOPLE);
 		peopleLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
 
+		recordSlider = new JSlider(JSlider.HORIZONTAL, 1, 10, RECORD_RATE);
+		recordSlider.setMajorTickSpacing(1);
+		recordSlider.setMinorTickSpacing(1);
+		recordSlider.setPaintTicks(true);
+		recordSlider.setPaintLabels(true);
+		recordSlider.setAlignmentX(Component.LEFT_ALIGNMENT);
+		recordSlider.addChangeListener(sliderListener);
+		recordSlider.setEnabled(false);
+
+		recordLabel = new JLabel("Record rate: " + RECORD_RATE);
+		recordLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+		immobileSlider = new JSlider(JSlider.HORIZONTAL, 0, PEOPLE, IMMOBILE);
+		immobileSlider.setMajorTickSpacing(250);
+		immobileSlider.setMinorTickSpacing(50);
+		immobileSlider.setPaintTicks(true);
+		immobileSlider.setPaintLabels(true);
+		immobileSlider.setAlignmentX(Component.LEFT_ALIGNMENT);
+		immobileSlider.addChangeListener(sliderListener);
+		immobileSlider.setEnabled(false);
+
+		immobileLabel = new JLabel("Immobile people: " + IMMOBILE);
+		immobileLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+		capacitySlider = new JSlider(JSlider.HORIZONTAL, 0, PEOPLE, CAPACITY);
+		capacitySlider.setMajorTickSpacing(250);
+		capacitySlider.setMinorTickSpacing(20);
+		capacitySlider.setPaintTicks(true);
+		capacitySlider.setPaintLabels(true);
+		capacitySlider.setAlignmentX(Component.LEFT_ALIGNMENT);
+		capacitySlider.addChangeListener(sliderListener);
+		capacitySlider.setEnabled(false);
+
+		capacityLabel = new JLabel("Hospital capacity: " + CAPACITY);
+		capacityLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
 		controlLabel = new JLabel("Controls");
 		controlLabel.setFont(new Font("Consolas", Font.BOLD, 40));
 
@@ -105,6 +141,9 @@ public class CoronaPanel extends JPanel {
 		restartButton.addActionListener(new ControlListener());
 
 		controls.add(controlLabel);
+		controls.add(pauseButton);
+		controls.add(playButton);
+		controls.add(restartButton);
 		controls.add(Box.createRigidArea(new Dimension(0, 30)));
 		controls.add(scaleLabel);
 		controls.add(scaleSlider);
@@ -115,9 +154,14 @@ public class CoronaPanel extends JPanel {
 		controls.add(peopleLabel);
 		controls.add(peopleSlider);
 		controls.add(Box.createRigidArea(new Dimension(0, 30)));
-		controls.add(pauseButton);
-		controls.add(playButton);
-		controls.add(restartButton);
+		controls.add(recordLabel);
+		controls.add(recordSlider);
+		controls.add(Box.createRigidArea(new Dimension(0, 30)));
+		controls.add(immobileLabel);
+		controls.add(immobileSlider);
+		controls.add(Box.createRigidArea(new Dimension(0, 30)));
+		controls.add(capacityLabel);
+		controls.add(capacitySlider);
 
 		BoxLayout layout = new BoxLayout(this, BoxLayout.X_AXIS);
 		setLayout(layout);
@@ -154,7 +198,7 @@ public class CoronaPanel extends JPanel {
 				page.setColor(Color.gray);
 				break;
 			}
-			if (timerTriggers % RECORD_RATE == 0 && people[i].getStatus() != 4) {
+			if (timerTriggers % recordRate == 0 && people[i].getStatus() != 4) {
 				graph[people[i].getStatus()][tick]++;
 			}
 			page.fillOval((int) people[i].getX(), (int) people[i].getY(), 10, 10);
@@ -184,7 +228,7 @@ public class CoronaPanel extends JPanel {
 				page.setColor(Color.gray);
 				break;
 			}
-			if (timerTriggers % RECORD_RATE == 0 && peopleDistanced[i].getStatus() != 4) {
+			if (timerTriggers % recordRate == 0 && peopleDistanced[i].getStatus() != 4) {
 				graphDistanced[peopleDistanced[i].getStatus()][tick]++;
 			}
 			page.fillOval((int) peopleDistanced[i].getX(), (int) peopleDistanced[i].getY(), 10, 10);
@@ -219,8 +263,8 @@ public class CoronaPanel extends JPanel {
 			page.drawLine(i, HEIGHT, i, temp);
 		}
 		page.setColor(Color.black);
-		page.drawLine(0, HEIGHT + ((numPeople - CAPACITY) / graphScale), WIDTH,
-				HEIGHT + ((numPeople - CAPACITY) / graphScale));
+		page.drawLine(0, HEIGHT + ((numPeople - capacity) / graphScale), WIDTH,
+				HEIGHT + ((numPeople - capacity) / graphScale));
 
 		// Draw graph (distanced)
 		for (int i = WIDTH + 1; i < WIDTH + tick; i++) {
@@ -246,10 +290,11 @@ public class CoronaPanel extends JPanel {
 			page.drawLine(i, Math.min(temp, HEIGHT), i, temp);
 		}
 		page.setColor(Color.black);
-		page.drawLine(WIDTH, HEIGHT + ((numPeople - CAPACITY) / graphScale), WIDTH * 2,
-				HEIGHT + ((numPeople - CAPACITY) / graphScale));
+		page.drawLine(WIDTH, HEIGHT + ((numPeople - capacity) / graphScale), WIDTH * 2,
+				HEIGHT + ((numPeople - capacity) / graphScale));
 
 		page.drawLine(WIDTH, 0, WIDTH, HEIGHT + (numPeople / graphScale));
+		page.drawLine(WIDTH * 2 - 1, 0, WIDTH * 2 - 1, HEIGHT + (numPeople / graphScale));
 	}
 
 	public void reinitialize() {
@@ -266,9 +311,9 @@ public class CoronaPanel extends JPanel {
 					i == infected ? true : false, false, false);
 		}
 
-		infected = (int) (Math.random() * (numPeople - IMMOBILE)) + IMMOBILE;
+		infected = (int) (Math.random() * (numPeople - immobile)) + immobile;
 		for (int i = 0; i < people.length; i++) {
-			if (i < IMMOBILE) {
+			if (i < immobile) {
 				peopleDistanced[i] = new Person((int) (Math.random() * (WIDTH - 10)) + WIDTH,
 						(int) (Math.random() * (HEIGHT - 10)), false, true, true);
 			} else {
@@ -276,29 +321,44 @@ public class CoronaPanel extends JPanel {
 						(int) (Math.random() * (HEIGHT - 10)), i == infected ? true : false, false, true);
 			}
 		}
-		playButton.setEnabled(true);
-		pauseButton.setEnabled(true);
-		restartButton.setEnabled(true);
-		scaleSlider.setEnabled(false);
-		timerSlider.setEnabled(true);
-		peopleSlider.setEnabled(false);
+
 	}
 
 	private class SliderListener implements ChangeListener {
 
 		public void stateChanged(ChangeEvent e) {
-			if (e.getSource() == scaleSlider || e.getSource() == peopleSlider) {
+			if (e.getSource() == scaleSlider || e.getSource() == peopleSlider || e.getSource() == recordSlider
+					|| e.getSource() == immobileSlider || e.getSource() == capacitySlider) {
 				playButton.setEnabled(false);
 				pauseButton.setEnabled(false);
 			}
+			if (e.getSource() == peopleSlider) {
+				immobileSlider.setValue(numPeople / 2);
+				immobileSlider.setMaximum(numPeople);
+
+				capacitySlider.setValue(numPeople / 2);
+				capacitySlider.setMaximum(numPeople);
+			}
+
 			graphScale = scaleSlider.getValue();
 			scaleLabel.setText("Graph Scaling: " + graphScale);
 			Corona.frame.setSize(new Dimension(WIDTH * 2 + LAYOUT_WIDTH + 16, HEIGHT + (numPeople / graphScale) + 39));
+
 			timer.setDelay(timerSlider.getValue());
 			timerLabel.setText("Tickrate: " + timerSlider.getValue());
 
 			numPeople = peopleSlider.getValue();
 			peopleLabel.setText("People: " + numPeople);
+
+			recordRate = recordSlider.getValue();
+			recordLabel.setText("Record rate: " + recordRate);
+
+			immobile = immobileSlider.getValue();
+			immobileLabel.setText("Immobile people: " + immobile);
+
+			capacity = capacitySlider.getValue();
+			capacityLabel.setText("Hospital capacity: " + capacity);
+
 		}
 	}
 
@@ -311,13 +371,26 @@ public class CoronaPanel extends JPanel {
 			} else if (e.getSource() == pauseButton) {
 				timer.stop();
 				paused = true;
-			} else {
-				timer.restart();
+			} else if (e.getSource() == restartButton) {
 				reinitialize();
+				paused = false;
+				playButton.setEnabled(true);
+				pauseButton.setEnabled(true);
+				restartButton.setEnabled(true);
+				scaleSlider.setEnabled(false);
+				timerSlider.setEnabled(true);
+				peopleSlider.setEnabled(false);
+				recordSlider.setEnabled(false);
+				immobileSlider.setEnabled(false);
+				capacitySlider.setEnabled(false);
+				timer.restart();
 			}
 
 			scaleSlider.setEnabled(paused);
 			peopleSlider.setEnabled(paused);
+			recordSlider.setEnabled(paused);
+			immobileSlider.setEnabled(paused);
+			capacitySlider.setEnabled(paused);
 
 		}
 
@@ -327,13 +400,13 @@ public class CoronaPanel extends JPanel {
 		public void actionPerformed(ActionEvent e) {
 			for (int i = 0; i < people.length; i++) {
 				people[i].tick();
-				if (people[i].isInfected() && i < PEOPLE * 0.2 && graph[2][i] < CAPACITY) {
+				if (people[i].isInfected() && i < PEOPLE * 0.2 && graph[2][i] < capacity) {
 					people[i].hospitalize();
 				} else if (people[i].isInfected() && i < PEOPLE * 0.2) {
 					people[i].kill();
 				}
 				peopleDistanced[i].tick();
-				if (peopleDistanced[i].isInfected() && i < PEOPLE * 0.2 && graphDistanced[2][i] < CAPACITY) {
+				if (peopleDistanced[i].isInfected() && i < PEOPLE * 0.2 && graphDistanced[2][i] < capacity) {
 					peopleDistanced[i].hospitalize();
 				} else if (peopleDistanced[i].isInfected() && i < PEOPLE * 0.2) {
 					peopleDistanced[i].kill();
@@ -341,7 +414,7 @@ public class CoronaPanel extends JPanel {
 			}
 			repaint();
 			timerTriggers++;
-			if (timerTriggers % RECORD_RATE == 0) {
+			if (timerTriggers % recordRate == 0) {
 				tick++;
 			}
 			if (tick > 599) {
